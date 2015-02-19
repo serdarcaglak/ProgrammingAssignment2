ab790653a77bd515f313008af93d6f645dce8c13
serdarcaglak <serdarcaglak@users.noreply.github.com>
2015-02-18 20:46:47 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Therefore, caching the inverse of a matrix may be beneficial
 ## rather than computing it simultaneously.
 ## Following code is given for caching the inverse of a matrix.  
-	 
+
 	## makeCacheMatrix function creates a special matrix object
 	## that can cache its inverse, which is really a list containing
 	## a function to:  
@@ -13,7 +13,7 @@
 		## set the value of the inverse of the matrix,
 		## get the value of the inverse of the matrix.
 ## My code for this assignment is given as follows:
-		
+
     makeCacheMatrix <- function(Serdar=matrix()) {
     Caglak <- NULL           # This is where the result of inversion is stored
     set <- function(FSU) {
@@ -30,7 +30,7 @@
     setInverse = setInverse,
     getInverse = getInverse)
      }
-     
+
 ## cacheSolve: This function computes the inverse of the special
 ## "matrix" returned by makeCacheMatrix above, unless the inverse 
 ## has already been calculated, in such cases it retrieves it from
@@ -46,14 +46,14 @@
     Serdar$setInverse(Caglak)             # then set it to the object
     Caglak                                # return the solved result
     }
-	
+
   ## Following code is used to test the both makeCacheMatrix 
 	## and cacheSolve Functions. Let`s use a 2x2 matrix called "Serdar". 
 	## If the inverse of the matrix Serdar has already obtained, it gets
   ## the inverse of Serdar from cache and skips the computation; 
 	## if not, it calculates the inverse of the matrix and sets the inverse
   ## in the cache via the setInverse function. 	
-	
+
     USA <- makeCacheMatrix(Serdar=matrix(9:12,2))
     USA$get() 
     USA$getInverse()
@@ -62,4 +62,3 @@
     cacheSolve(USA)
     cacheSolve(USA)        # to check if the message is delivered
     USA$getInverse()       # to set the inversed matrix
-    FL = USA$getInverse()  # to get the inversed matrix
